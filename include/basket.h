456ff4ad9b6608870da986098e0a602d1dd22e6e
lebed <lebed@DESKTOP-BKIICKL>
2022-10-13 12:24:21 +0300

    Add project files.

---
+++ include/basket.h
@@ -0,0 +1,112 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include "throws.h"
+
+
+class Basket
+{
+public:
+    
+    void showDatabase()
+    {
+        for (auto it = database.begin(); it != database.end(); it++)
+        {
+            std::cout << it->first << " " << it->second << std::endl;
+        }
+    }
+
+    void showBasket()
+    {
+        for (auto it = basket.begin(); it != basket.end(); it++)
+        {
+            std::cout << it->first << " " << it->second << std::endl;
+        }
+    }
+
+    void add(std::string& inArticle, int inAmount)
+    {
+        auto productOnDatabase = database.find(inArticle);
+        if (productOnDatabase == database.end())
+        {
+            throw Error::ARTICLE_NOT_FOUND;
+        }
+        else
+        {
+            int amountOnDatabase = productOnDatabase->second;
+            if (amountOnDatabase < inAmount)
+            {
+                throw Error::NOT_ENOUGH_AMOUNT;
+            }   
+            auto productOnBasket = basket.find(inArticle);
+            if (productOnBasket != basket.end())
+            {
+                int amountOnBasket = productOnBasket->second;
+
+                if (amountOnDatabase < amountOnBasket + inAmount)
+                {
+                    throw Error::NOT_ENOUGH_AMOUNT;
+                }
+                else
+                {
+                    productOnBasket->second += inAmount;
+                    productOnDatabase->second -= inAmount;
+                }
+            }
+            else
+            {
+                basket.insert(std::make_pair(inArticle, inAmount));
+                productOnDatabase->second -= inAmount;
+            }
+        }   
+    }
+
+    void remove(std::string& inArticle, int inAmount)
+    {
+        auto productOnBasket = basket.find(inArticle);
+        if (productOnBasket == basket.end())
+        {
+            throw Error::ARTICLE_NOT_FOUND;
+        }
+            
+        int amountProduct = productOnBasket->second;
+        if (amountProduct < inAmount)
+        {
+            throw Error::NOT_ENOUGH_AMOUNT;
+        }   
+        else
+            if (amountProduct == inAmount)
+            {
+                basket.erase(inArticle);
+            }  
+            else
+            {
+                productOnBasket->second -= inAmount;
+            }
+                
+        std::cout << "Product was deleted from basket" << std::endl << std::endl;
+
+        auto productOnDatabase = database.find(inArticle);
+        if (productOnDatabase == database.end())
+        {
+            throw Error::ARTICLE_NOT_FOUND;
+        }
+        else
+        {
+            productOnDatabase->second += inAmount;
+        }  
+    }
+
+    
+    
+private:
+    std::map <std::string, int> database = {{"000001", 10},
+                                            {"000002", 9},
+                                            {"000003", 8},
+                                            {"000004", 10}};
+
+    std::map <std::string, int> basket;
+};
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <map>
+#include <fstream>
+#include <boost/asio.hpp>
+#include "basket.h"
+#include "throws.h"
+
+
+
+
+int main()
+{
+    
+    Basket basket;
+   
+    bool exit = false;
+    while (!exit)
+    {
+        system("cls");
+        std::cout << "ONLINE STORE" << std::endl;
+        basket.showDatabase();
+        std::cout << std::endl;
+        std::cout << "BASKET" << std::endl;
+        basket.showBasket();
+        std::cout << std::endl;
+        std::cout << "Choose one of action: " << std::endl;
+        std::cout << "1. Add product to basket;" << std::endl;
+        std::cout << "2. Remove product from basket;" << std::endl;
+        std::cout << "0. Exit from shop." << std::endl;
+
+        int chosen;
+        std::cin >> chosen;
+
+        std::string article = "";
+        int amount = 0;
+        if (chosen == 1 || chosen == 2)
+        {
+            std::cout << "Enter product article:";
+            std::cin >> article;
+            std::cout << "Enter product amount:";
+            std::cin >> amount;
+        }
+        try
+        {
+            switch (chosen)
+            {
+            case 1: basket.add(article, amount);
+            {
+
+                break;
+            }
+            case 2: basket.remove(article, amount); break;
+            case 0: exit = true;                    break;
+            default: std::cout << "! Incorrect choose. Try again !";
+            }
+        }
+        catch (Error myErr)
+        {
+            boost::asio::io_context io;
+            boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+            
+            switch (myErr)
+            {
+            case Error::ARTICLE_NOT_FOUND:
+                
+                std::cerr << "This article doesn't found" << std::endl;
+                
+                t.wait();
+                break;
+
+            case Error::NOT_ENOUGH_AMOUNT:
+                
+                std::cerr << "Not enough amount" << std::endl;
+                
+                t.wait();
+                break;
+            }
+        }
+    }
+}
